3b9c4657c75d9a9b839e68ce31aade2dbcbf81fe
johna.ca8146 <johna.ca8146@vusd.us>
2022-03-30 15:22:12 -0700

    fixed....EVERYTHING

--- app/src/main/java/com/example/mybotany/FirstFragment.java
+++ app/src/main/java/com/example/mybotany/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.mybotany;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,12 +9,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mybotany.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private RecyclerView mRecyclerView;
+    private ArrayList<Plant> mPlantData;
+    private AdapterClass mAdapter;
 
     @Override
     public View onCreateView(
@@ -21,9 +29,37 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
+        //initializes array list
+        mPlantData = new ArrayList<>();
+
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        //initialize recycler view and sets layout manager
+        mRecyclerView = binding.plantTimerList;
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+
         return binding.getRoot();
 
+    }
+
+    private void initializeData() {
+        String[] plantNameList = getResources()
+                .getStringArray(R.array.plant_names);
+        String[] plantInfo = getResources()
+                .getStringArray(R.array.plant_info);
+        TypedArray plantImageResources = getResources().obtainTypedArray(R.array.plant_images);
+
+        mPlantData.clear();
+
+        for (int i = 0; i < plantNameList.length; i++ ) {
+            mPlantData.add(new Plant(plantNameList[i], plantInfo[i], plantImageResources.getResourceId(i, 0)));
+        }
+        plantImageResources.recycle();
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void resetPlants (View view) {
+        initializeData();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
--- app/src/main/java/com/example/mybotany/MainActivity.java
+++ app/src/main/java/com/example/mybotany/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,12 +29,7 @@ import java.util.LinkedList;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
     private FloatingActionButton addPlantButton;
-
-    private RecyclerView mRecyclerView;
-    private ArrayList<Plant> mPlantData;
-    private AdapterClass mAdapter;
 
     private final LinkedList<String> mPlantList = new LinkedList<>();
 
@@ -44,21 +38,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //initialize recycler view and sets layout manager
-        mRecyclerView = findViewById(R.id.recyclerView);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        //initializes array list
-        mPlantData = new ArrayList<>();
-
-        setContentView(R.layout.activity_main);
-
         addPlantButton = findViewById(R.id.addPlantButton);
         Toolbar toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavController navController =Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
@@ -71,26 +56,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-    private void initializeData() {
-        String[] plantNameList = getResources()
-                .getStringArray(R.array.plant_names);
-        String[] plantInfo = getResources()
-                .getStringArray(R.array.plant_info);
-        TypedArray plantImageResources = getResources().obtainTypedArray(R.array.plant_images);
-
-        mPlantData.clear();
-
-        for (int i = 0; i < plantNameList.length; i++ ) {
-            mPlantData.add(new Plant(plantNameList[i], plantInfo[i], plantImageResources.getResourceId(i, 0)));
-        }
-        plantImageResources.recycle();
-        mAdapter.notifyDataSetChanged();
-    }
-
-    private void resetPlants (View view) {
-        initializeData();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
