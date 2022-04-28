c38e4f64d43153723447ae36123a5e9a1b24d36c
antho.cr0783 <antho.cr0783@vusd.us>
2022-04-28 15:15:35 -0700

    tried to mess with LiveData

--- app/src/main/java/com/example/mybotany/ButtonListAdapter.java
+++ app/src/main/java/com/example/mybotany/ButtonListAdapter.java
@@ -2,6 +2,7 @@ package com.example.mybotany;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,10 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -67,6 +71,46 @@ public class ButtonListAdapter extends RecyclerView.Adapter<ButtonListAdapter.Vi
                 }
             });
         }
+    }
 
+    public class SelectedPlantViewModel extends ViewModel {
+
+        // Create a LiveData with a String
+        private MutableLiveData<String> currentName;
+        private MutableLiveData<String> currentInfo;
+        private MutableLiveData<String> currentWaterInfo;
+        private MutableLiveData<Drawable> currentPic;
+
+        public MutableLiveData<String> getCurrentName() {
+            return currentName;
+        }
+
+        public void setCurrentName(MutableLiveData<String> currentName) {
+            this.currentName = currentName;
+        }
+
+        public MutableLiveData<String> getCurrentInfo() {
+            return currentInfo;
+        }
+
+        public void setCurrentInfo(MutableLiveData<String> currentInfo) {
+            this.currentInfo = currentInfo;
+        }
+
+        public MutableLiveData<String> getCurrentWaterInfo() {
+            return currentWaterInfo;
+        }
+
+        public void setCurrentWaterInfo(MutableLiveData<String> currentWaterInfo) {
+            this.currentWaterInfo = currentWaterInfo;
+        }
+
+        public MutableLiveData<Drawable> getCurrentPic() {
+            return currentPic;
+        }
+
+        public void setCurrentPic(MutableLiveData<Drawable> currentPic) {
+            this.currentPic = currentPic;
+        }
     }
 }
